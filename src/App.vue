5129a580cd40c21e301d04ab1c0de9df68306354
meng25290 <2529035579@qq.com>
2023-11-23 09:56:26 +0800

    import element-ui & i18n

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <template>
   <div id="app">
+    {{ $t('index.hello') }}
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      
+    }
+  },
+  mounted() {
+    //通过浏览器自动切换国际化语言版本
+    console.log("语言：" + navigator.language);
+    let lang = navigator.language;
+    if (lang) {
+      this.$i18n.locale = lang;
+    } else {
+      this.$i18n.locale = 'en'
+    }
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+// i18n
+import i18n from './i18n';
+
 Vue.config.productionTip = false
 
 new Vue({
+  i18n,
   router,
   store,
   render: h => h(App)
